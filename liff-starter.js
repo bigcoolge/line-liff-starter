09e5f433f1027358c87ffe46f842e64504170986
Juliano Penna <julianopenna@gmail.com>
2019-05-31 15:14:52 +0800

    Create phone form

--- liff-starter.js
+++ liff-starter.js
@@ -1,88 +1,53 @@
 window.onload = function (e) {
-    liff.init(function (data) {
-        initializeApp(data);
-    });
+  liff.init(function () {
+    console.log('init')
+    liff.getProfile()
+      .then(function (profile) {
+        initializeApp(profile.userId, profile.displayName);
+      })
+      .catch(function (err) {
+        window.alert('Something wrong. Please try again later');
+        console.error(err);
+      });
+  });
 };
+initializeApp('id', 'john');
 
-function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
+function initializeApp(userId, displayName) {
+  console.log('app')
+  // closeWindow call
+  document.getElementById('close').addEventListener('click', function () {
+    liff.closeWindow();
+  });
 
-    // openWindow call
-    document.getElementById('openwindowbutton').addEventListener('click', function () {
-        liff.openWindow({
-            url: 'https://line.me'
-        });
-    });
+  // Send form
+  document.getElementById('phoneForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    var phoneNumber = e.target.elements['phone'].value;
 
-    // closeWindow call
-    document.getElementById('closewindowbutton').addEventListener('click', function () {
-        liff.closeWindow();
-    });
-
-    // sendMessages call
-    document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        liff.sendMessages([{
-            type: 'text',
-            text: "You've successfully sent a message! Hooray!"
-        }, {
-            type: 'sticker',
-            packageId: '2',
-            stickerId: '144'
-        }]).then(function () {
-            window.alert("Message sent");
-        }).catch(function (error) {
-            window.alert("Error sending message: " + error);
-        });
-    });
-
-    // get access token
-    document.getElementById('getaccesstoken').addEventListener('click', function () {
-        const accessToken = liff.getAccessToken();
-        document.getElementById('accesstokenfield').textContent = accessToken;
-        toggleAccessToken();
-    });
-
-    // get profile call
-    document.getElementById('getprofilebutton').addEventListener('click', function () {
-        liff.getProfile().then(function (profile) {
-            document.getElementById('useridprofilefield').textContent = profile.userId;
-            document.getElementById('displaynamefield').textContent = profile.displayName;
-
-            const profilePictureDiv = document.getElementById('profilepicturediv');
-            if (profilePictureDiv.firstElementChild) {
-                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-            }
-            const img = document.createElement('img');
-            img.src = profile.pictureUrl;
-            img.alt = "Profile Picture";
-            profilePictureDiv.appendChild(img);
-
-            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-            toggleProfileData();
-        }).catch(function (error) {
-            window.alert("Error getting profile: " + error);
-        });
-    });
-}
-
-function toggleAccessToken() {
-    toggleElement('accesstokendata');
-}
-
-function toggleProfileData() {
-    toggleElement('profileinfo');
-}
-
-function toggleElement(elementId) {
-    const elem = document.getElementById(elementId);
-    if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-        elem.style.display = "none";
-    } else {
-        elem.style.display = "block";
+    if (!/^\d{8}$/.test(phoneNumber)) {
+      return window.alert('Please type a valid phone number (only numbers, at least 8 digits)');
     }
+
+    const data = [
+      ['userId', userId],
+      ['name', displayName],
+      ['phone', phoneNumber],
+    ].map(([field, value]) => field + '=' + value)
+    .join('&');
+
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('load', function () {
+      if (this.readyState === 4 && this.status === 200) {
+        window.alert('Registered!');
+        liff.closeWindow();
+      } else {
+        window.alert('Could\'t process your registration. Please try again later');
+      }
+    });
+
+    request.open('GET', 'http://tom-line-msg.herokuapp.com/api/push?' + data, true);
+    request.send();
+  });
 }
