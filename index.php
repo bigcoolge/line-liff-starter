e58d8d754b888d94dd43194296ef8df36907e160
Juliano Penna <julianopenna@gmail.com>
2019-05-31 15:32:10 +0800

    disable cache

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <!-- This file allows you to host this page as a static file on Heroku -->
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php
+  header( 'Location: /index.html' );
+  header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
+?>
