c9233bae3ad94dff620623f00677f13c294e2b37
Juliano Penna <julianopenna@gmail.com>
2019-05-31 16:10:18 +0800

    Handle errors

--- liff-starter.js
+++ liff-starter.js
@@ -1,52 +1,59 @@
-window.onload = function (e) {
-  liff.init(function () {
-    console.log('liff initialized.')
-    liff.getProfile()
-    .then(function (profile) {
-        console.log('Got profile.')
-        initializeApp(profile.userId, profile.displayName);
-      })
-      .catch(function (err) {
-        window.alert('Something wrong. Please try again later');
-      });
-  });
-};
+var userId = 'U55dbae93eeaee85433eeb60d77461e0b';
+var displayName = 'Marc';
 
-function initializeApp(userId, displayName) {
-  console.log('Initializing app.')
+// Send form
+document.getElementById('phoneForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  var phoneNumber = e.target.elements['phone'].value;
 
-  // Send form
-  document.getElementById('phoneForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    var phoneNumber = e.target.elements['phone'].value;
+  if (!/^\d{8}$/.test(phoneNumber)) {
+    return window.alert('Please type a valid phone number (only numbers, at least 8 digits)');
+  }
 
-    if (!/^\d{8}$/.test(phoneNumber)) {
-      return window.alert('Please type a valid phone number (only numbers, at least 8 digits)');
+  const data = [
+    ['userId', userId],
+    ['name', displayName],
+    ['phone', phoneNumber],
+  ].map(([field, value]) => field + '=' + value)
+  .join('&');
+
+  const request = new XMLHttpRequest();
+
+  request.addEventListener('load', function (evt) {
+    if (this.readyState === 4 && this.status === 200) {
+      window.alert('Registered!');
+      liff.closeWindow();
+    } else {
+      window.alert('Could\'t process your registration. Please try again later.');
     }
-
-    const data = [
-      ['userId', userId],
-      ['name', displayName],
-      ['phone', phoneNumber],
-    ].map(([field, value]) => field + '=' + value)
-    .join('&');
-
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('load', function () {
-      if (this.readyState === 4 && this.status === 200) {
-        window.alert('Registered!');
-        liff.closeWindow();
-      } else {
-        window.alert('Could\'t process your registration. Please try again later');
-      }
-    });
-
-    request.addEventListener('error', function () {
-      window.alert('Could\'t process your registration. Please try again later');
-    });
-
-    request.open('GET', 'https://tom-line-msg.herokuapp.com/api/push?' + data, true);
-    request.send();
   });
-}
+
+  request.addEventListener('error', function (evt) {
+    window.alert('Request error. Please try again later.');
+  });
+
+  request.open('GET', 'https://tom-line-msg.herokuapp.com/api/push?' + data, true);
+  request.send();
+});
+
+window.onload = function () {
+  // Line
+  try {
+    liff.init(function () {
+      liff.getProfile()
+        .then(function (profile) {
+          userId = profile.userId
+          displayName = profile.displayName;
+          var button = document.getElementById('submitButton')
+          button.removeAttribute('disabled');
+          button.innerHTML('Register');
+        })
+        .catch(function (err) {
+          window.alert('Profile fetching error. Please try again later.');
+        });
+      });
+    } catch (err) {
+      console.error(err);
+      window.alert('Initialization error. Please try again later.');
+  }
+};
